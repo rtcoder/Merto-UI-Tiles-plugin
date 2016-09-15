54194ef83f6ef074b6e19909189cf3c692d391da
Dawid <dawidjez@gmail.com>
2016-09-16 00:23:06 +0200

    documentation

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
 	$('pre code').each(function(i, block) {
 		hljs.highlightBlock(block);
 	});
+	// $('a').click(function(){
+	// 	if($(this).attr('href').indexOf('/') < 0){
+	// 		window.location.href = $(this).attr('href')
+	// 		var url = document.URL;
+	// 		if(url.indexOf('#') >= 0){
+	// 			var URLarr = url.split('#');
+	// 			var id = '#'+URLarr[1];
+	// 			$('a').removeClass('current-link')
+	// 			$('a[href=\\'+id+']').addClass('current-link');
+	// 		}
+	// 	}
+	// });
 })
