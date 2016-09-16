954e0a99e5466830667d377d2504a987ba7d628a
Dawid <dawidjez@gmail.com>
2016-09-17 00:28:08 +0200

    update documentation

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
 	$('pre code').each(function(i, block) {
 		hljs.highlightBlock(block);
 	});
+	
+	var inter = setInterval(function(){
+		if($('iframe').length > 0){
+			var url = document.URL;
+			$('iframe').each(function(){
+				if(url.indexOf('#') >= 0){
+					window.location.href = document.URL;
+				}
+			})
+			clearInterval(inter)
+		}
+	},1)
 })
